40c5c8909498fde2b5c02fdf606d05c2a738ee38
shreyzeous21 <shrey.sadhukhan21@gmail.com>
2024-04-29 01:20:53 +0530

    Messenger_fullstack_done

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import { Inter } from "next/font/google";
+
+import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
+import ActiveStatus from "./components/ActiveStatus";
+
+import "./globals.css";
+import LoadingLogo from "./components/LoadingLogo";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Messenger | DEV",
+  description: "Messenger Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
+      </head>
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+        <LoadingLogo />
+      </body>
+    </html>
+  );
+}
